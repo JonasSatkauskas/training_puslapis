b29e2234523eb4fc1a4b87829d45582d0473891d
JonasSatkauskas <joncius224@gmail.com>
2021-01-12 22:44:47 +0200

    Second comit

--- webpage/aplikacija/views.py
+++ webpage/aplikacija/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def namai(request):
+    my_dict = {'tagas': ''}
+    return render(request, 'aplikacija/index.html', context=my_dict)
